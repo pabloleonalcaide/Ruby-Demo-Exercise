a4778e03f824dc105919a6664370761a99f42ade
pabloleonalcaide <pabloleonpsico@gmail.com>
2019-07-14 18:50:39 +0200

    Added models validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,7 @@
 class User < ActiveRecord::Base
     has_many :microposts
+    validates :user , length: { maximum: 50, minimum: 5 }, 
+                       presence: true
+    validates :email, length: {minimum: 7}, presence: true
+  
 end
